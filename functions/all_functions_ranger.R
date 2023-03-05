b1db44405b0be213db05621fd13a45614a8cea3e
John Llewelyn <johnsllewelyn@gmail.com>
2023-03-06 09:25:36 +1030

    Add files via upload

--- functions/all_functions_ranger.R
+++ functions/all_functions_ranger.R
@@ -42,6 +42,7 @@ train_test_abs <- function(x,y,z,abs) {
   y <- y[y$sourceTaxonName%in%unique(obs$sourceTaxonName),]
   unobs <- y[sample(nrow(y), dim(obs)[1]*abs, replace = FALSE),] #unobs for training
   unobs$interact <- as.factor(FALSE)
+  unobs$outside <- NULL
   data <- rbind(obs,unobs)
   sample = sample.split(data$interact, SplitRatio = .75)
   train = subset(data, sample == TRUE)
@@ -51,6 +52,7 @@ train_test_abs <- function(x,y,z,abs) {
   unobs2   = unobs2[!(paste(unobs2$sourceTaxonName,unobs2$targetTaxonName)%in%paste(unobs$sourceTaxonName,unobs$targetTaxonName)),]
   unobs2   = unobs2[sample(nrow(unobs2),dim(test_obs)[1], replace = FALSE),]
   unobs2$interact <- as.factor(FALSE)
+  unobs2$outside <- NULL
   test = rbind(test_obs,unobs2)
   train_w <- train$interact
   train_w <- ifelse(train_w=="TRUE",1,1/abs)
@@ -995,7 +997,7 @@ rf_prob <- function(dt_train,dt_test,mtr){
   return(res)}
 
 
-#identify columns with taxonomic information based on teh presence of "Name"
+#identify columns with taxonomic information based on the presence of "Name"
 tax_cols <- function (x) { which(grepl("Name",names(x)))}
 
 #identify columns with taxonomic information based on the presence of "Name", 
@@ -1654,3 +1656,160 @@ all_imp <- function(x,y,z,ins,out,abs,thresh,mtr, ...) {
   return(imp)
 }
 
+
+#build training and testing datasets, with pre-defined composition/connectance
+train_test_conn <- function(x,y,z,ins,out,conn) { #ins and out = how many times more from inside versus outside suitable size range e.g., 10 and 1 or 1 and 10, abs = how many times more absent than present
+  obs <- x
+  obs$interact <- as.factor(TRUE)
+  obs$outside = "present"
+  y <- y[y$sourceTaxonName%in%unique(obs$sourceTaxonName),]
+  unobs_in <- y[y$outside=="FALSE",]
+  unobs_in <- unobs_in[sample(nrow(unobs_in), (dim(obs)[1]/(ins+out)*ins)*((1-conn)/conn), replace = FALSE),] #unobs for training
+  unobs_out <- y[y$outside=="TRUE",]
+  unobs_out <- unobs_out[sample(nrow(unobs_out), (dim(obs)[1]/(ins+out)*out)*((1-conn)/conn), replace = FALSE),]
+  unobs <- rbind(unobs_in,unobs_out)
+  #unobs$outside = NULL
+  unobs$interact <- as.factor(FALSE)
+  data <- rbind(obs,unobs[,names(obs)])
+  sample = sample.split(data$interact, SplitRatio = .75)
+  train = subset(data, sample == TRUE)
+  test_obs = subset(data, sample == FALSE)
+  test_obs = test_obs[test_obs$interact=="TRUE",]
+  unobs2   = z
+  unobs2   = unobs2[!(paste(unobs2$sourceTaxonName,unobs2$targetTaxonName)%in%paste(unobs$sourceTaxonName,unobs$targetTaxonName)),]
+  unobs2   = unobs2[sample(nrow(unobs2),dim(test_obs)[1]*((1-conn)/conn), replace = FALSE),]
+  unobs2$interact <- as.factor(FALSE)
+  #unobs2$outside = NULL
+  test = rbind(test_obs,unobs2)
+  test$outside = NULL
+  train$outside <- ifelse(train$outside=="present",1,1/((1-conn)/conn))
+  train_w <- train$outside
+  train$outside = NULL
+  return(list(train,test,train_w))
+}
+
+#unweighted but with specified composition/connectance
+rep_all_nw_conn <- function(x,y,z,ins,out,conn,thresh,mtr, ...) {
+  ch <- train_test_conn(x,y,z,ins,out,conn)
+  ch[[1]] <- ch[[1]][,-tax_cols(ch[[1]])]
+  ch[[2]] <- ch[[2]][,-tax_cols(ch[[2]])]
+  rf <- ranger(formula = interact ~ ., # - targetTaxonGenusName - sourceTaxonGenusName, 
+               data = ch[[1]],mtry=mtr,num.threads=20,probability = T) #
+  pred <- predict(rf, data=ch[[2]][,-(which(names(ch[[2]])%in%c("interact","sourceTaxonName","targetTaxonName")))])
+  #scores = predic$predictions[,1]
+  #lbls <- dt_test$interact
+  #lbls <- ifelse(lbls=="TRUE",1,0)
+  #all_auc <- auc(scores, lbls)
+  pred = pred$predictions[,1] > thresh
+  pred_perf <- table(ch[[2]][,which(names(ch[[2]])=="interact")], pred)
+  all_tss <- round(tss(pred_perf),3)
+  #list(all_tss, all_auc)
+  return(all_tss)
+}
+
+
+#unweighted and taking expected connectance into account when building training data
+rf_thresh_nw_conn <- function(x,y,ins,out,conn,dt_test, thresh,mtr,...){
+  obs <- x
+  obs$interact <- as.factor(TRUE)
+  obs$outside = "present"
+  y <- y[y$sourceTaxonName%in%unique(obs$sourceTaxonName),]
+  unobs_in <- y[y$outside=="FALSE",]
+  unobs_in <- unobs_in[sample(nrow(unobs_in), (dim(obs)[1]/(ins+out)*ins)*((1-conn)/conn), replace = FALSE),] #unobs for training
+  unobs_out <- y[y$outside=="TRUE",]
+  unobs_out <- unobs_out[sample(nrow(unobs_out), (dim(obs)[1]/(ins+out)*out)*((1-conn)/conn), replace = FALSE),]
+  unobs <- rbind(unobs_in,unobs_out)
+  unobs$obs = NULL
+  unobs$interact <- as.factor(FALSE)
+  data <- rbind(obs,unobs)
+  #data_w <- ifelse(data$interact=="TRUE",1,1/((1-conn)/conn))
+  data$outside = NULL
+  data = data[,-tax_cols(data)]  #ch[[2]] <- ch[[2]][,-tax_cols(ch[[2]])]
+  #dt_test = dt_test[,!(tax_cols_sp(dt_test))]
+  #try so there's an equal number observed vs unobserved in test data
+  dt_ob <- dt_test[dt_test$interact=="TRUE",]
+  dt_unob <- dt_test[dt_test$interact=="FALSE",]
+  #dt_unob <- dt_unob[sample(nrow(dt_unob),dim(dt_ob)[1]),]
+  dt_test <- rbind(dt_ob,dt_unob)
+  #dt_test <- dt_test[,-tax_cols(dt_test)] #can include species names or remove them, doesn't make a difference
+  #dt_test = dt_test[,!(names(dt_test)%in%dist)]
+  rf = ranger(formula = interact ~., 
+              data = data,
+              #num.trees = 500, 
+              mtry = mtr, 
+              num.threads = 20, 
+              probability = T, 
+              importance = 'impurity',
+              #case.weights = data_w
+  )
+  predic = predict(rf, data=dt_test[,-(which(names(dt_test)%in%c("interact","sourceTaxonName","targetTaxonName")))])
+  scores = predic$predictions[,1]
+  lbls <- dt_test$interact
+  lbls <- ifelse(lbls=="TRUE",1,0)
+  all_auc <- auc(scores, lbls)
+  predic = predic$predictions[,1] > thresh
+  #res <- data.frame(cbind(as.logical(dt_test$interact),predic))
+  pred_perf <- table(dt_test$interact, predic)
+  all_tss <- round(tss(pred_perf),3)
+  list(Tsk=c(all_tss),Ac=c(all_auc))
+}
+
+
+#to replicate on global and specify tree depth
+rep_all_mdepth <- function(x,y,z,ins,out,abs,thresh,mtr,mdepth, ...) {
+  ch <- train_test_dfsw(x,y,z,ins,out,abs)
+  ch[[1]] <- ch[[1]][,-tax_cols(ch[[1]])]
+  ch[[2]] <- ch[[2]][,-tax_cols(ch[[2]])]
+  rf <- ranger(formula = interact ~ ., # - targetTaxonGenusName - sourceTaxonGenusName, 
+               data = ch[[1]],num.tree=800,mtry=mtr,num.threads=20,probability = T,case.weights = ch[[3]],max.depth = mdepth) #
+  pred <- predict(rf, data=ch[[2]][,-(which(names(ch[[2]])%in%c("interact","sourceTaxonName","targetTaxonName")))])
+  #scores = predic$predictions[,1]
+  #lbls <- dt_test$interact
+  #lbls <- ifelse(lbls=="TRUE",1,0)
+  #all_auc <- auc(scores, lbls)
+  pred = pred$predictions[,1] > thresh
+  pred_perf <- table(ch[[2]][,which(names(ch[[2]])=="interact")], pred)
+  all_tss <- round(tss(pred_perf),3)
+  #list(all_tss, all_auc)
+  return(all_tss)
+}
+
+###same as above but for Simp
+rf_thresh_allmdepth <- function(x,y,ins,out,abs,dt_test, thresh,mtr,mdepth,...){
+  obs <- x
+  obs$interact <- as.factor(TRUE)
+  obs$outside = "present"
+  y <- y[y$sourceTaxonName%in%unique(obs$sourceTaxonName),]
+  unobs_in <- y[y$outside=="FALSE",]
+  unobs_in <- unobs_in[sample(nrow(unobs_in), (dim(obs)[1]/(ins+out)*ins)*abs, replace = FALSE),] #unobs for training
+  unobs_out <- y[y$outside=="TRUE",]
+  unobs_out <- unobs_out[sample(nrow(unobs_out), (dim(obs)[1]/(ins+out)*out)*abs, replace = FALSE),]
+  unobs <- rbind(unobs_in,unobs_out)
+  unobs$obs = NULL
+  unobs$interact <- as.factor(FALSE)
+  data <- rbind(obs,unobs)
+  data_w <- ifelse(data$interact=="TRUE",1,1/abs)
+  data$outside = NULL
+  data = data[,-tax_cols(data)]  #ch[[2]] <- ch[[2]][,-tax_cols(ch[[2]])]
+  dt_test = dt_test[,names(data)]
+  #dt_test <- dt_test[,-tax_cols(dt_test)] #can include species names or remove them, doesn't make a difference
+  dt_test$interact <- as.factor(dt_test$interact)
+  rf = ranger(formula = interact ~., 
+              data = data,
+              mtry = mtr, 
+              num.threads = 20, 
+              probability = T, 
+              importance = 'impurity',
+              case.weights = data_w,
+              max.depth = mdepth)
+  predic = predict(rf, data=dt_test[,-(which(names(dt_test)%in%c("interact","sourceTaxonName","targetTaxonName")))])
+  scores = predic$predictions[,1]
+  lbls <- dt_test$interact
+  lbls <- ifelse(lbls=="TRUE",1,0)
+  all_auc <- auc(scores, lbls)
+  predic = predic$predictions[,1] > thresh
+  #res <- data.frame(cbind(as.logical(dt_test$interact),predic))
+  pred_perf <- table(dt_test$interact, predic)
+  all_tss <- round(tss(pred_perf),3)
+  list(Tsk=c(all_tss),Ac=c(all_auc))
+}
